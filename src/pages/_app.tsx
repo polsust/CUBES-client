68e7fa89e843ae9a6cc782c4e0cd5cd1bdd5daa3
polsust <polsust@gmail.com>
2023-03-15 14:22:41 +0100

    Move providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,8 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Providers from "@providers/Providers";
 import type { AppProps } from "next/app";
+import Providers from "@common/providers/Providers";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
