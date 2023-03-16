469e994b803e6e191a93d9b879e8d33580758275
polsust <polsust@gmail.com>
2023-03-16 10:22:51 +0100

    Update import syntax

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import BottomTabs from "@common/components/BottomTabs";
-import Mobile from "@common/components/MediaQueries/Mobile";
+import BottomTabs from "@cubes/common/components/BottomTabs";
+import Mobile from "@cubes/common/components/MediaQueries/Mobile";
 import "@styles/globals.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
-import Providers from "@common/providers/Providers";
+import Providers from "@cubes/common/providers/Providers";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
