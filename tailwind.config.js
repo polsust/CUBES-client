fc3e3fe9260dbaa4e94c9f94cedaae682d8f32ed
polsust <polsust@gmail.com>
2023-03-17 10:22:39 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/containers/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#10AEA5",
-      secondary: "#23272A",
-      info: "#738BD7",
+    extend: {
+      colors: {
+        primary: "#10AEA5",
+        secondary: "#23272A",
+        info: "#738BD7",
+      },
     },
-    extend: {},
   },
   plugins: [],
   corePlugins: {
