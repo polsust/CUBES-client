1e6a3e356b38b97175a71276dc0676c220d99145
polsust <polsust@gmail.com>
2023-04-30 12:17:31 +0200

    Adapt mobile media query to match tailwind

--- src/common/components/MediaQueries/Mobile.tsx
+++ src/common/components/MediaQueries/Mobile.tsx
@@ -1,7 +1,5 @@
 import ChildrenProps from "@cubes/common/types/ChildrenProps";
-import React from "react";
-import MediaQuery from "react-responsive";
 
 export default function Mobile({ children }: ChildrenProps) {
-  return <MediaQuery maxWidth={600}>{children}</MediaQuery>;
+  return <div className="md:hidden">{children}</div>;
 }
