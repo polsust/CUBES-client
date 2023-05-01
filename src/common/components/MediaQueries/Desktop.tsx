f4d747693cd7017b13ba2a1d77618eafc874c333
polsust <polsust@gmail.com>
2023-05-01 20:18:58 +0200

    Adapt desktop media query to match tailwind

--- src/common/components/MediaQueries/Desktop.tsx
+++ src/common/components/MediaQueries/Desktop.tsx
@@ -1,7 +1,5 @@
 import ChildrenProps from "@cubes/common/types/ChildrenProps";
-import React from "react";
-import MediaQuery from "react-responsive";
 
 export default function Desktop({ children }: ChildrenProps) {
-  return <MediaQuery minWidth={1200}></MediaQuery>;
+  return <div className="md:block hidden">{children}</div>;
 }
