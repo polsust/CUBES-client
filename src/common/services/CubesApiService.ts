717b5123bcaa4baa6e122aaa7b406a8e79a78790
polsust <polsust@gmail.com>
2023-05-07 17:56:22 +0200

    Create a wrapper for the api client

---
+++ src/common/services/CubesApiService.ts
@@ -0,0 +1,40 @@
+import { message } from "antd";
+import axios from "axios";
+import {
+  Configuration,
+  UserDtoResponse,
+  UserApi,
+  RolesApi,
+  CategoryApi,
+  CommentsApi,
+  RessourceApi,
+  AuthApi,
+} from "cubes-api-client";
+
+export function cubesApiService() {
+  const conf = new Configuration({
+    basePath: "https://localhost:5001",
+  });
+
+  const axiosInstance = axios.create();
+
+  axiosInstance.interceptors.response.use((res) => {
+    // all responses are the same
+    const dto = res.data as UserDtoResponse;
+
+    if (res.status.toString()[0] !== "2") {
+      message.error({ content: dto.message });
+    }
+
+    return res;
+  });
+
+  return {
+    user: new UserApi(conf, undefined, axiosInstance),
+    roles: new RolesApi(conf, undefined, axiosInstance),
+    category: new CategoryApi(conf, undefined, axiosInstance),
+    comments: new CommentsApi(conf, undefined, axiosInstance),
+    ressource: new RessourceApi(conf, undefined, axiosInstance),
+    auth: new AuthApi(conf, undefined, axiosInstance),
+  };
+}
