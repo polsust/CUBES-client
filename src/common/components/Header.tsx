081dd9fa1dbbc39e865fa4676cf62751ffecdcb5
polsust <polsust@gmail.com>
2023-05-08 11:51:04 +0200

    Redirect to root route on logo click

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,18 +1,22 @@
 import { Button } from "antd";
 import Image from "next/image";
+import Link from "next/link";
+import { ROUTES } from "../constants";
 
 interface HeaderProps { }
 
 export default function Header({ }: HeaderProps) {
   return (
-    <div className="flex fixed top-0 z-10 justify-between items-center my-5 px-5 w-full">
+    <div className="flex fixed top-0 z-10 justify-between items-center px-5 my-5 w-full">
       <div>
-        <Image
-          src="/assets/images/logo.png"
-          alt="logo"
-          height={100}
-          width={100}
-        />
+        <Link href={ROUTES.root}>
+          <Image
+            src="/assets/images/logo.png"
+            alt="logo"
+            height={100}
+            width={100}
+          />
+        </Link>
       </div>
       <div>
         <Button type="default" size="large">
--- src/common/constants.ts
+++ src/common/constants.ts
@@ -7,4 +7,5 @@ export const COLORS = {
 export enum ROUTES {
   login = "/connexion",
   signup = "/inscription",
+  root = "/",
 }
