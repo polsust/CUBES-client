045f8d5dc30904cf839daa213c395523c02ec6f2
polsust <polsust@gmail.com>
2023-05-18 11:36:20 +0200

    Use id for fething resources

--- src/pages/catalogue/[id].tsx
+++ src/pages/catalogue/[id].tsx
@@ -6,11 +6,12 @@ import { IResource } from "@cubes/modules/resource/types/Resource";
 export const getServerSideProps: GetServerSideProps<{
   resource: IResource;
 }> = async (context) => {
-  const reference = context?.params?.reference as string;
+  const id = context?.params?.id as string;
 
   const instance = axios.create();
   const res = await instance.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/Ressource/GetRessource/${encodeURIComponent(reference)}`
+    `${process.env.NEXT_PUBLIC_API_URL
+    }/api/Ressource/GetRessource/${id}`
   );
 
   return {
