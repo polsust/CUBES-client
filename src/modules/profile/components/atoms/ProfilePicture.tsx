460dd6f50871cba8fb1625f33f6eeabbc8fdeec9
polsust <polsust@gmail.com>
2023-05-20 19:01:15 +0200

    Add sizing to profile picture

--- src/modules/profile/components/atoms/ProfilePicture.tsx
+++ src/modules/profile/components/atoms/ProfilePicture.tsx
@@ -2,14 +2,22 @@ import { JwtContent } from "@cubes/modules/auth/types/Jwt";
 
 interface ProfilePictureProps {
   user: JwtContent;
+  size?: "sm" | "md" | "lg";
 }
 
-export default function ProfilePicture({ user }: ProfilePictureProps) {
+export default function ProfilePicture({
+  user,
+  size = "md",
+}: ProfilePictureProps) {
   const fNameInital = user.firstname.at(0);
   const lNameInital = "fake lastname".at(0);
 
   return (
-    <div className="relative w-40 h-40 text-6xl text-white capitalize rounded-full md:w-52 md:h-52 md:text-8xl bg-blue-400">
+    <div
+      className={`relative text-white capitalize bg-blue-400 rounded-full ${size == "sm" && "w-16 h-16 text-4xl"
+        } ${size == "md" && "w-32 h-32 text-7xl"} ${size == "lg" && "w-48 h-48 text-8xl"
+        } `}
+    >
       <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         {fNameInital}
         {lNameInital}
