b8c52069e8a06aaf1082d0f08ba8d822729ddb5f
polsust <polsust@gmail.com>
2023-05-21 16:47:30 +0200

    Fix pp color

--- src/modules/profile/components/atoms/ProfilePicture.tsx
+++ src/modules/profile/components/atoms/ProfilePicture.tsx
@@ -16,35 +16,38 @@ export default function ProfilePicture({
   const fullNameLength = user.fName.length + user.lName.length;
 
   const colors = [
-    "slate",
-    "gray",
-    "zinc",
-    "neutral",
-    "stone",
-    "red",
-    "orange",
-    "amber",
-    "yellow",
-    "lime",
-    "green",
-    "emerald",
-    "teal",
-    "cyan",
-    "sky",
-    "blue",
-    "indigo",
-    "violet",
-    "purple",
-    "fuchsia",
-    "pink",
-    "rose",
+    "#94a3b8",
+    "#9ca3af",
+    "#a1a1aa",
+    "#a3a3a3",
+    "#a8a29e",
+    "#f87171",
+    "#fb923c",
+    "#fbbf24",
+    "#facc15",
+    "#a3e635",
+    "#4ade80",
+    "#34d399",
+    "#2dd4bf",
+    "#22d3ee",
+    "#38bdf8",
+    "#60a5fa",
+    "#818cf8",
+    "#a78bfa",
+    "#c084fc",
+    "#e879f9",
+    "#f472b6",
+    "#fb7185",
   ];
 
   const color = useMemo(() => colors[fullNameLength], [fullNameLength]);
 
   return (
     <div
-      className={`relative text-white capitalize bg-${color}-400 rounded-full aspect-square ${size == "sm" && "w-16 h-16 text-4xl"
+      style={{
+        backgroundColor: color,
+      }}
+      className={`relative text-white capitalize rounded-full aspect-square ${size == "sm" && "w-16 h-16 text-4xl"
         } ${size == "md" && "w-32 h-32 text-7xl"} ${size == "lg" && "w-48 h-48 text-8xl"
         } `}
     >
