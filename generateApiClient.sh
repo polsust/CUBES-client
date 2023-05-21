04b229d3821eadbc0dee4971134d4f1b862c96fc
polsust <polsust@gmail.com>
2023-05-21 12:36:53 +0200

    Complete script for generating api client

--- generateApiClient.sh
+++ generateApiClient.sh
@@ -1,1 +1,11 @@
-openapi-generator-cli generate -g typescript-axios -i spec.json -o cubes-api-client --additional-properties=npmName=cubes-api-client,supportsES6=true,withoutPrefixEnums=true,npmVersion="$(npm view cubes-api-client version)",withInterfaces=true
+#!/usr/bin/env sh
+
+wget --no-check-certificate https://localhost:5001/swagger/v1/swagger.json
+
+openapi-generator-cli generate -g typescript-axios -i swagger.json -o cubes-api-client --additional-properties=npmName=cubes-api-client,supportsES6=true,withoutPrefixEnums=true,npmVersion="$(npm view cubes-api-client version)",withInterfaces=true
+
+cd cubes-api-client || exit
+
+npm install
+npm version patch
+npm publish
