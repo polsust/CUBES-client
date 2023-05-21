cfa51555a16f2c113eb01a691762963ec8622704
polsust <polsust@gmail.com>
2023-05-21 16:00:38 +0200

    Give different colors for users

--- src/modules/profile/components/atoms/ProfilePicture.tsx
+++ src/modules/profile/components/atoms/ProfilePicture.tsx
@@ -1,4 +1,5 @@
 import { User } from "@cubes/modules/auth/types/Jwt";
+import { useMemo } from "react";
 
 interface ProfilePictureProps {
   user: User;
@@ -12,9 +13,38 @@ export default function ProfilePicture({
   const fNameInital = user.fName.at(0);
   const lNameInital = user.lName.at(0);
 
+  const fullNameLength = user.fName.length + user.lName.length;
+
+  const colors = [
+    "slate",
+    "gray",
+    "zinc",
+    "neutral",
+    "stone",
+    "red",
+    "orange",
+    "amber",
+    "yellow",
+    "lime",
+    "green",
+    "emerald",
+    "teal",
+    "cyan",
+    "sky",
+    "blue",
+    "indigo",
+    "violet",
+    "purple",
+    "fuchsia",
+    "pink",
+    "rose",
+  ];
+
+  const color = useMemo(() => colors[fullNameLength], [fullNameLength]);
+
   return (
     <div
-      className={`relative text-white capitalize bg-blue-400 rounded-full aspect-square ${size == "sm" && "w-16 h-16 text-4xl"
+      className={`relative text-white capitalize bg-${color}-400 rounded-full aspect-square ${size == "sm" && "w-16 h-16 text-4xl"
         } ${size == "md" && "w-32 h-32 text-7xl"} ${size == "lg" && "w-48 h-48 text-8xl"
         } `}
     >
